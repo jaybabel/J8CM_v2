0f4f432200db5a65dd46eda6a47dc169d411c119
Jay E. Babel <jaybabel@gmail.com>
2022-04-09 12:51:11 -0500

    PSW bit, color coding, formating

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -28,24 +28,32 @@ void MainWindow::on_pushButton_Step_clicked()
 void MainWindow::on_pushButton_Fetch_clicked()
 {
     ui->label_2->setText("Fetch Cycle");
+    ui->radioButton_Fetch->setCheckable(true);
+    ui->radioButton_Execute->setChecked(false);
+    ui->radioButton_Fetch->setChecked(true);
+//    ui->radioButton_Fetch->setCheckable(false);
+    //set PSW bit 7 to zero after the fetch cycle
+    ui->radioButton_PSW7->setChecked(true);
 }
 
 void MainWindow::on_pushButton_Execute_clicked()
 {
     ui->label_2->setText("Execute Cycle");
+    ui->radioButton_Execute->setCheckable(true);
+    ui->radioButton_Fetch->setChecked(false);
+    ui->radioButton_Execute->setChecked(true);
+//    ui->radioButton_Execute->setCheckable(false);
+    //set PSW bit 7 to one after the execute cycle
+    ui->radioButton_PSW7->setChecked(false);
 }
 
 void MainWindow::on_pushButton_Mode_clicked()
 {
     ui->label_2->setText(ui->comboBox_Mode->currentText());
-//    QMessageBox::information(this, "Item Selection",
-//                                 ui->comboBox_Mode->currentText());
+    //QMessageBox::information(this, "Item Selection", ui->comboBox_Mode->currentText());
 }
-
 
 void MainWindow::on_horizontalSlider_Clock_sliderMoved()
 {
     ui->lcdNumber->display(ui->horizontalSlider_Clock->sliderPosition());
-//    ui->lcdNumber->display(ui->horizontalSlider_Clock->value());
-//    ui->label_2->setText("Clock Speed");
 }
